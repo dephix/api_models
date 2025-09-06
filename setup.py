f30ae4f04ce79ac5c70add28509c4437876377c0
andreyfesunov <fsnv04@gmail.com>
2025-09-06 16:08:38 +0300

    chore(release): bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="dephix-api-models",
-    version="0.1.6",
+    version="0.1.7",
     author="Dephix",
     description="Models and interfaces for trading APIs",
     long_description=long_description,
